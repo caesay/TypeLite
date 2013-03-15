8d37801242c9c174d5f096e3b9194648baed31fe
Lukas Kabrt <Lukas Kabrt@localhost>
2013-03-15 22:54:02 +0100

    Added demo project

---
+++ TypeLite.Demo/Models/Order.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TypeLite.Demo.Models {
+	[TsClass(Module = "Eshop")]
+	public class Order {
+		public Product[] Products { get; set; }
+		public decimal TotalPrice { get; set; }
+	}
+}
---
+++ TypeLite.Demo/Scripts/TypeLite.d.ts
@@ -0,0 +1,20 @@
+﻿
+ 
+
+
+module Eshop {
+interface Product {
+  Name: string;
+  Price: number;
+}
+interface Order {
+  Products: Product[];
+  TotalPrice: number;
+}
+interface Customer {
+  Name: string;
+  Email: string;
+  VIP: bool;
+  Orders: Order[];
+}
+}

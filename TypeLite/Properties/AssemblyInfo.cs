193d32ad9ad6673ec10509dc40d968a8e318b8a9
pooya_samizadeh <pooya_samizadeh@TheRealPooya.ApexSI.local>
2014-06-09 13:51:04 -0400

    Created snk files. Now TypeLite can be used in projects with "Sign the assembly" checked.

---
+++ TypeLite/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TypeLite")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TypeLite")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.9.1.7")]
+[assembly: AssemblyFileVersion("0.9.1.7")]
+[assembly: InternalsVisibleTo("TypeLite.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100df9defcb717cb5" +
+                                                        "79cf724afd36bb140f421806385e1d8f4f869833dfe4e2865e0fa45341d73b1dbe8de8b2540bec" +
+                                                        "9b93a6e219e8a796a391e2b8612a8f837cfa99fd14e1efb93d93f865728283ee3f70852d12e39b" +
+                                                        "70dea6999152821cc9b7c7f8b14d45ad10f6506df64f5929b77366c9b07b64f4a592b8affe09da" +
+                                                        "8800238c")]

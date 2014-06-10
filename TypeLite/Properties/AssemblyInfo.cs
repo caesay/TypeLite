01fb0733d8906e62208ffe7c958ce1bf84096c52
Lukas Kabrt <Lukas Kabrt@localhost>
2014-06-10 17:46:55 +0200

    Merge with default

--- TypeLite/Properties/AssemblyInfo.cs
+++ TypeLite/Properties/AssemblyInfo.cs
@@ -24,7 +24,10 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.1.8")]
-[assembly: AssemblyFileVersion("0.9.1.8")]
-
-[assembly: InternalsVisibleTo("TypeLite.Tests")]
+[assembly: AssemblyVersion("0.9.1.9")]
+[assembly: AssemblyFileVersion("0.9.1.9")]
+[assembly: InternalsVisibleTo("TypeLite.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100d9351cdf0fc79c" +
+                                                        "633b745a17c690b062462a709fe67aa221fbbdd2e8c3a966198544713fdbd4715d2f3992bf5c6d" + 
+                                                        "5b64fae15ad44e75583549a921dd378f55a785a41f3fff9f10825f4154c932a3fae6ca0004afd4" + 
+                                                        "7bfdeaeb0b4d9679a1e3a829780804896220726299fa652a644b948adb82f48d349125b27c50c4" +
+                                                        "9b2521f1")]

911642837e7de56095b7f79a70f0a031d169a3e5
Lukas Kabrt <Lukas Kabrt@localhost>
2014-09-05 18:35:47 +0200

    Issue #52 - Enum without class

--- TypeLite/TsModelBuilder.cs
+++ TypeLite/TsModelBuilder.cs
@@ -113,7 +113,9 @@ namespace TypeLite {
 		/// </summary>
 		/// <param name="assembly">The assembly with classes to add</param>
 		public void Add(Assembly assembly) {
-			foreach (var type in assembly.GetTypes().Where(t => t.GetCustomAttribute<TsClassAttribute>(false) != null && TsType.GetTypeFamily(t) == TsTypeFamily.Class)) {
+			foreach (var type in assembly.GetTypes().Where(t => 
+                (t.GetCustomAttribute<TsClassAttribute>(false) != null && TsType.GetTypeFamily(t) == TsTypeFamily.Class) ||
+                (t.GetCustomAttribute<TsEnumAttribute>(false) != null && TsType.GetTypeFamily(t) == TsTypeFamily.Enum))) {
 				this.Add(type);
 			}
 		}

cc49bcce31c3365582eaf80391893ac64224d2db
kasajima <kasajima@sunengin.co.jp>
2015-06-30 08:56:40 +0900

    enable DocumentationFile of demo project.

---
+++ TypeLite.Demo/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TypeLite.Demo.Models {
+	[TsClass(Module = "Eshop")]
+	public class Order {
+		public Product[] Products { get; set; }
+		public decimal TotalPrice { get; set; }
+		public DateTime Created { get; set; }
+        public DateTimeOffset Shipped { get; set; }
+	}
+}
---
+++ TypeLite.Demo/Scripts/TypeLite.d.ts
@@ -0,0 +1,50 @@
+﻿
+
+ 
+
+
+
+/// <reference path="Enums.ts" />
+
+declare module Eshop {
+  interface Customer {
+    Name: string;
+    Email: string;
+    VIP: boolean;
+    Kind: Eshop.CustomerKind;
+    Orders: Eshop.Order[];
+  }
+  interface Order {
+    Products: Eshop.Product[];
+    TotalPrice: number;
+    Created: Date;
+    Shipped: Date;
+  }
+  interface Product {
+    Name: string;
+    Price: number;
+    ID: System.Guid;
+  }
+}
+declare module System {
+  interface Guid {
+  }
+}
+declare module TypeLite.Demo.Models {
+  interface IShippingService {
+    Price: number;
+  }
+}
+declare module Library {
+  interface Book {
+    Title: string;
+    Pages: number;
+    Genre: Library.Genre;
+  }
+  interface Library {
+    Name: string;
+    Books: Library.Book[];
+  }
+}
+
+

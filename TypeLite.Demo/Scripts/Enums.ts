d5143c76a5cca98d4049150b0e52c4107f1a178e
Paul O'Neill <hello@pablissimo.com>
2016-01-29 11:23:51 +0000

    Created new branch issue82

---
+++ TypeLite.Demo/Scripts/Enums.ts
@@ -0,0 +1,37 @@
+module Eshop {
+
+  /**
+   * Customer Kind
+   */
+  export enum CustomerKind {
+
+    /**
+     * Corporate customer
+     */
+    Corporate = 1,
+
+    /**
+     * Individual customer
+     */
+    Individual = 2
+  }
+}
+module Library {
+
+  /**
+   * genre definitions
+   */
+  export enum Genre {
+
+    /**
+     * science fiction
+     */
+    Scifi = 1,
+
+    /**
+     * course book
+     */
+    Coursebook = 2
+  }
+}
+
---
+++ TypeLite.Demo/Scripts/TypeLite.d.ts
@@ -0,0 +1,146 @@
+﻿
+
+ 
+
+
+
+/// <reference path="Enums.ts" />
+
+declare module Eshop {
+
+  /**
+   * Customer information
+   */
+  interface Customer {
+
+    /**
+     * Customer name.
+     */
+    Name: string;
+
+    /**
+     * Email address.
+     */
+    Email: string;
+
+    /**
+     * Customer is VIP member or not.
+     */
+    VIP: boolean;
+
+    /**
+     * Customer's kind.
+     */
+    Kind: Eshop.CustomerKind;
+
+    /**
+     * Customer's orders.
+     */
+    Orders: Eshop.Order[];
+  }
+
+  /**
+   * a order
+   */
+  interface Order {
+
+    /**
+     * products
+     */
+    Products: Eshop.Product[];
+
+    /**
+     * total price
+     */
+    TotalPrice: number;
+
+    /**
+     * created date
+     */
+    Created: Date;
+
+    /**
+     * shipped date
+     */
+    Shipped: Date;
+  }
+
+  /**
+   * product
+   */
+  interface Product {
+
+    /**
+     * name
+     */
+    Name: string;
+
+    /**
+     * price
+     */
+    Price: number;
+
+    /**
+     * ID
+     */
+    ID: System.Guid;
+  }
+}
+declare module System {
+  interface Guid {
+  }
+}
+declare module TypeLite.Demo.Models {
+
+  /**
+   * Shipping Service
+   */
+  interface IShippingService {
+
+    /**
+     * price
+     */
+    Price: number;
+  }
+}
+declare module Library {
+
+  /**
+   * Book
+   */
+  interface Book {
+
+    /**
+     * name
+     */
+    Title: string;
+
+    /**
+     * pages of book
+     */
+    Pages: number;
+
+    /**
+     * genre
+     */
+    Genre: Library.Genre;
+  }
+
+  /**
+   * book library
+   */
+  interface Library {
+
+    /**
+     * name
+     */
+    Name: string;
+
+    /**
+     * books in library
+     */
+    Books: Library.Book[];
+  }
+}
+
+
